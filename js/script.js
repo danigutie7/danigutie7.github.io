0ceab89b7ac8d56005fcb305044dc81268666c76
danigutie7 <danielgutie6@gmail.com>
2023-08-07 23:02:04 -0500

    ft
    Actualización ficha técnica

---
+++ js/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var pantallaCarga = document.getElementById('pantalla-carga');
+    var contenido = document.getElementById('contenido');
+
+    pantallaCarga.style.display = 'none';
+    contenido.style.display = 'block';
+}
